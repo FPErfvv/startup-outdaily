61d15916d7ccf5a82eafd4ac696e0b728a19163e
Fred Probst <github@fsd.probst.org>
2026-02-19 09:25:10 -0700

    continues moving files around

---
+++ src/login/forgotPassword.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useUser } from '../UserContext';
+export function ForgotPassword() {
+    const { email, setEmail } = useUser();
+    const { currentPage, setCurrentPage } = useUser();
+    return (
+        <div className="bd-example border border-3 rounded-top-5 flex-fill flex-column m-3 col-md-10 shadow">
+            <h3 className="py-4 text-center bg-success bg-opacity-50 rounded-top-5 border border-3">Start tracking every experience with nature.</h3>
+            <form className="px-4 py-3" action="entry.html">
+                <div className="mb-3">
+                    <label htmlFor="exampleDropdownFormEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com" onChange={(e)=>(setEmail(e.target.value))} />
+                </div>
+                <div className="mb-3"></div>
+                <div className="d-flex justify-content-center">
+                        <button type="submit" className="btn btn-success px-5">Send Reset Link</button>
+                </div>
+                
+            </form>
+            <div className="dropdown-divider border"></div>
+            <div className="text-center my-1">
+                <a className="dropdown-item m-1 text-decoration-underline d-inline" onClick={() => setCurrentPage('unauthenticated')}>Back to Login</a>
+            </div>
+
+        </div>
+  );
+}
