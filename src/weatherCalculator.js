710ee46c338c8536abaa7958b64d14201cecc214
Fred Probst <github@fsd.probst.org>
2026-03-12 22:37:38 -0600

    fixes bug with weather data type

--- src/weatherCalculator.js
+++ src/weatherCalculator.js
@@ -1,29 +1,29 @@
 const weights = {
     temp: 10,
-    cloudConditions: 10,
-    chanceOfRain: 20,
-    humidity: 2,
+    cloud_cover: 10,
+    precipitation: 20,
+    relative_humidity_2m: 2,
     duration: 30
 }
 export function calculatePoints(weatherInfo, duration) {
-    const { temp, cloudConditions, chanceOfRain, humidity } = weatherInfo;
+    const { temperature_2m, cloud_cover, precipitation, relative_humidity_2m } = weatherInfo;
     let points = 0;
-    let normalizedTemp = (temp + 10)/50;
-    let normalizedChanceOfRain = chanceOfRain/100;
-    let normalizedHumidity = humidity/100;
-    let normalizedCloudConditions = cloudConditions / 100;
+    let normalizedTemp = (parseFloat(temperature_2m) + 10)/50;
+    let normalizedPrecipitation = parseFloat(precipitation)/100;
+    let normalizedRelative_humidity_2m = parseFloat(relative_humidity_2m)/100;
+    let normalizedCloud_cover = parseFloat(cloud_cover) / 100;
     let normalizedDuration = duration / 60;
 
     console.log("normalizedTemp: ", normalizedTemp);
-    console.log("normalizedCloudConditions: ", normalizedCloudConditions);
-    console.log("normalizedChanceOfRain: ", normalizedChanceOfRain);
-    console.log("normalizedHumidity: ", normalizedHumidity);
+    console.log("normalizedCloudConditions: ", normalizedCloud_cover);
+    console.log("normalizedChanceOfRain: ", normalizedPrecipitation);
+    console.log("normalizedHumidity: ", normalizedRelative_humidity_2m);
     console.log("normalizedDuration: ", normalizedDuration);
 
-    points += normalizedCloudConditions * weights.cloudConditions;
+    points += normalizedCloud_cover * weights.cloud_cover;
     points += normalizedTemp * weights.temp;
-    points += normalizedChanceOfRain * weights.chanceOfRain;
-    points += normalizedHumidity * weights.humidity;
+    points += normalizedPrecipitation * weights.precipitation;
+    points += normalizedRelative_humidity_2m * weights.relative_humidity_2m;
     points += normalizedDuration * weights.duration;
     return Math.round(points);
 }
