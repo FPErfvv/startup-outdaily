80ec7749e574ffae03ee96647a91bf4a5f5795b1
Fred Probst <github@fsd.probst.org>
2026-04-07 21:30:07 -0600

    deletes un-used code

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { getLeaderboard } from '../service';
 import { GameNotifier } from '../gameNotifier';
 import { useUser } from '../UserContext';
 import { useNavigate } from 'react-router-dom';
--- src/service.js
+++ src/service.js
@@ -1,15 +1,5 @@
 // The following functions handle the transfer of user info
 
-export function updateDBonLogout(username) {
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    const user = getUser(users, username = username, "");
-    if (user) {
-        user.loggedIn = false;
-        localStorage.setItem('users', JSON.stringify(users));
-        return { success: true, message: 'Logout successful' };
-    }
-}
-
 
 export async function getUsername(email) {
     const res = await fetch('/api/user/me', {
@@ -32,54 +22,7 @@ export async function getUserInfo() {
         method: 'GET',
         credentials: 'include',
       });
-      return await res.json();
-}
-
-
-// The following function handles the retrieval of the leaderboard
-export function getLeaderboard() {
-    const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-    return leaderboard;
-}
-
-// The following function handles the simulation of the leaderboard, as if it were pulling from a database
-export function simulateLeaderboard() {
-    const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-    if (leaderboard.length < 2) {
-        leaderboard.push({ username: 'OutDailyPro', points: 1000, streak: 60 });
-        leaderboard.push({ username: 'TreeHugger', points: 800, streak: 50 });
-        leaderboard.push({ username: 'Bill', points: 300, streak: 20 });
-        leaderboard.push({ username: 'BestHiker26', points: 100, streak: 10 });
-        localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
-        return leaderboard;
-    }
-    return leaderboard;
-}
-
-export function updateLeaderboard(username, points, streak) {
-    const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-    const user = getUser(leaderboard, username = username, "");
-    if (!user) {
-        leaderboard.push({ username: username, points: points, streak: streak });
-    }
-    else {
-        user.points = points;
-        user.streak = streak;
-    }
-    localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
-    return leaderboard;
-}
-
-function getUser(users, username = "", email = "") {
-    if (username !== "") {
-        return users.find(user => user.username === username);
-    }
-    else if (email !== "") {
-        return users.find(user => user.email === email);
-    }
-    else {
-        return null;
-    }
+    return await res.json();
 }
 
 export async function getCoordinates(cityName) {
